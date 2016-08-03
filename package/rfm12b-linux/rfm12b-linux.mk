36136b72ccafb6db474529d327e27a39fb4f8872
Bert Outtier <outtierbert@gmail.com>
2016-08-03 18:43:45 +0200

    Initial commit

---
+++ package/rfm12b-linux/rfm12b-linux.mk
@@ -0,0 +1,8 @@
+RFM12B_LINUX_VERSION = master
+RFM12B_LINUX_SITE = https://github.com/gkaindl/rfm12b-linux.git
+RFM12B_LINUX_SITE_METHOD = git
+
+RFM12B_LINUX_INSTALL_STAGING = NO
+RFM12B_LINUX_INSTALL_TARGET = YES
+
+$(eval $(generic-package))
